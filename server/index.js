ee3d5ef13c065a5fa1e69ab8817a34c9177040f2
fernandes99 <fernandes.r1999@gmail.com>
2023-08-19 13:24:37 -0300

    set port 8080

--- server/index.js
+++ server/index.js
@@ -63,6 +63,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(80, () => {
-  console.log("SERVER IS RUNNING ON PORT 80");
+server.listen(8080, () => {
+  console.log("SERVER IS RUNNING ON PORT 8080");
 });
