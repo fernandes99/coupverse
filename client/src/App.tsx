0bfdbc45baf3a60a9c620878755ed37d9f5d0ec0
Roberto Fernandes <49538386+fernandes99@users.noreply.github.com>
2023-08-27 16:38:51 -0300

    fix lint

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import * as io from 'socket.io-client';
 
